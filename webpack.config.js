96899087aea2a7570f815f3f650bc8f602436214
Lucas-Lu <lydge1112@gmail.com>
2018-03-07 00:11:00 +0800

    update webConfig

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,34 @@ const path = require('path');
 var webpack = require('webpack');
 var Ex = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-
+var getHtmlConfig = function(name){
+  return {
+    template : './src/view/' + name + '.html',
+    filename : 'view/' + name + '.html',
+    inject : true,
+    hash : true,
+    chunks : ['common',name]
+  };
+};
 
 var config = {
     entry: {
-        "common":['./src/page/common/index.js'],
+        "common":['./src/page/common/index.js','webpack-dev-server/client?http://localhost:8088'],
         "index":['./src/page/index/index.js'],
         "login":['./src/page/login/index.js']
     },
     output: {
       path: path.resolve(__dirname, 'dist'),
+      publishPath: '/dist',
       filename: 'js/[name].js'
     },
     module: {
-      loaders: [{
-        test: /\.css/,
-        loader:
-        Ex.extract({fallback: "style-loader",use: "css-loader"})
+      loaders: [{test: /\.css/,loader:Ex.extract({fallback: "style-loader",use: "css-loader"})
         //loader: Ex.extract('style-loader', 'css-loader')  // 单独打包出CSS，这里配置注意下
         //loader: "style-loader!css-loader"  // 单独打包出CSS，这里配置注意下
-      }]
+      },
+      { test: /\.(gif|png|jpg|woff|svg|eto|ttf)\??.*$/, loader: 'url-loader?limit=100&name=resourcer/[name].[ext]'}
+      ] 
     },
     externals:{
       'jquery' : 'window.jQuery'
@@ -32,14 +40,8 @@ var config = {
         filename : 'js/base.js'
       }),
       new Ex("css/[name].css"),
-      new HtmlWebpackPlugin({
-          template : './src/view/index.html',
-          filename : 'view/index.html',
-          inject : true,
-          hash : true,
-          trunks : ['common','index']
-
-      })
+      new HtmlWebpackPlugin(getHtmlConfig('index')),
+      new HtmlWebpackPlugin(getHtmlConfig('login')),
     ]
   };
 
